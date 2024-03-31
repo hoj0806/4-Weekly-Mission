8bcbed6ad8497a9ec98c0368f4328803d8d2bf0e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-03-31 12:36:12 +0900

    docs : 라우팅 설정 및 기본 파일 구조 작성

---
+++ app/(FolderPage)/folder/page.tsx
@@ -0,0 +1,5 @@
+const FolderPage = () => {
+  return <h1>Folder Page</h1>;
+};
+
+export default FolderPage;
---
+++ app/(SharedPage)/shared/page.tsx
@@ -0,0 +1,5 @@
+const SharedPage = () => {
+  return <h1>Shared Page</h1>;
+};
+
+export default SharedPage;
