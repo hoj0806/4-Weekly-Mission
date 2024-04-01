00f722554377b94fcda07539c428c9ef035b7b72
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-04-02 00:50:10 +0900

    style : 홈페이지 스타일링

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -3,9 +3,19 @@ import Link from "next/link";
 const Home = () => {
   return (
     <>
-      <h1>HomePage</h1>
-      <Link href='/folder'>Folder 페이지로</Link>
-      <Link href='/shared'>Shared 페이지로</Link>
+      <h1 style={{ fontSize: "100px" }}>HomePage</h1>
+
+      <div>
+        <Link href='/folder' style={{ fontSize: "100px" }}>
+          Folder 페이지로
+        </Link>
+      </div>
+
+      <div>
+        <Link href='/shared' style={{ fontSize: "100px" }}>
+          Shared 페이지로
+        </Link>
+      </div>
     </>
   );
 };
